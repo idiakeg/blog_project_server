3709d103c1b5401c5cf7307d63921e2da7a95344
idiakeg <idiakeg@gmail.com>
2024-01-29 22:00:59 +0100

    implemented password encryption

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,5 +1,6 @@
 const { Schema, model } = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema(
   {
@@ -20,6 +21,17 @@ const userSchema = new Schema(
       required: [true, "Please enter a password"],
       minlength: 8,
     },
+    confirm_password: {
+      type: String,
+      required: [true, "Please confirm your password"],
+      validate: {
+        // this validator will only work for save and create, not when a document is to be updated.
+        validator: function (value) {
+          return value === this.password;
+        },
+        message: "Password and confirm_password do not match",
+      },
+    },
     no_of_posts: {
       type: Number,
       default: 0,
@@ -29,6 +41,20 @@ const userSchema = new Schema(
     collection: "blog_users",
   }
 );
+
+// password encryption before saving
+userSchema.pre("save", async function (next) {
+  // check if the the password was modified, if it wasnot, do nothing
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  // if it was modified, encrypt it
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // set the confirm password field to undefined so it is not saved in the database
+  this.confirm_password = undefined;
+});
 
 const userModel = model("User", userSchema);
 

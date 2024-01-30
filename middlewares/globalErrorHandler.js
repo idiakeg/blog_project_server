622dbed7d0fb56f1011fb60ca4409c009f919ad7
idiakeg <idiakeg@gmail.com>
2024-01-30 11:53:44 +0100

    duplicate key error

--- middlewares/globalErrorHandler.js
+++ middlewares/globalErrorHandler.js
@@ -14,15 +14,22 @@ const castErrorHandler = (err) => {
   return new customErrorHandler(msg, 400);
 };
 
+const duplicateKeyErrorHandler = (err) => {
+  const msg = `User with email: ${err.keyValue.email} already exists in the database. Try a different email`;
+  return new customErrorHandler(msg, 400);
+};
+
 const prodError = (res, err) => {
   // check if the error object has the isOperational property
   if (err.isOperational) {
     res.status(err.statusCode || 500).json({
+      status: "error",
       message: err.message || "Internal server error",
     });
   } else {
     /* if the error is not operational, or other forms of error, e.g the ones thrown by mongoose validator  */
     res.status(500).json({
+      status: "error",
       message: "Something went wrong, please try again later.",
     });
   }
@@ -40,6 +47,10 @@ const globalErrorHandler = (err, req, res, next) => {
     if (err.name === "castError") {
       // reassign the err object that was thrown by mongoose that had no status code, isOperational of false and gaunty error message to the sophisticated error message of the customErrorHandler
       err = castErrorHandler(err);
+    }
+    // if it is a duplicate key error
+    if (err.code === 11000) {
+      err = duplicateKeyErrorHandler(err);
     }
     prodError(res, err);
   }

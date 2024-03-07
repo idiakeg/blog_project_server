0d8a9ad4499e1344f52a90337cd78abca971e199
idiakeg <idiakeg@gmail.com>
2024-03-07 14:44:17 +0100

    delete post endpoint

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -116,11 +116,56 @@ const editPost = asyncErrorHandler(async (req, res, next) => {
     return next(new customErrorHandler("All fields are required.", 422));
   }
 
-  // make it possible to update a blog post without updating the thumbnail
-  if (!thumbnail) {
+  // get the actual blog post
+  const oldBlogPost = await postModel.findById(id);
+  // make sure that a user can only delete a post that they created.
+  if (req.user.id == oldBlogPost.creator) {
+    // make it possible to update a blog post without updating the thumbnail
+
+    if (!thumbnail) {
+      editedPost = await postModel.findByIdAndUpdate(
+        id,
+        { title, category, description },
+        { new: true }
+      );
+
+      if (!editedPost) {
+        return next(new customErrorHandler("Could not update post", 422));
+      }
+
+      return res.status(200).json(editedPost);
+    }
+
+    // if the user wants to also edit the thumbnail of the blog post
+    // delete the old thumbnail
+    removeExistingAvatar(oldBlogPost, "blog");
+    // upload a new thumbnail
+    // check file size
+    if (thumbnail.size > 2000000) {
+      return next(
+        new customErrorHandler(
+          "Thumbnail too large. Should be less than 2Mb",
+          422
+        )
+      );
+    }
+    // rename the file
+    const newFileName = fileRename(thumbnail);
+    //   move the file into the uploads folder
+    thumbnail.mv(path.join(__dirname, "..", "uploads", newFileName), (err) => {
+      if (err) {
+        return next(new customErrorHandler(err));
+      }
+    });
+    // update the blog post with the new details
     editedPost = await postModel.findByIdAndUpdate(
       id,
-      { title, category, description },
+      {
+        title,
+        description,
+        category,
+        thumbnail: newFileName,
+      },
       { new: true }
     );
 
@@ -131,51 +176,43 @@ const editPost = asyncErrorHandler(async (req, res, next) => {
     return res.status(200).json(editedPost);
   }
 
-  // if the user wants to also edit the thumbnail of the blog post
-  // get the actual blog post
-  const oldBlogPost = await postModel.findById(id);
-  // delete the old thumbnail
-  removeExistingAvatar(oldBlogPost, "blog");
-  // upload a new thumbnail
-  // check file size
-  if (thumbnail.size > 2000000) {
-    return next(
-      new customErrorHandler(
-        "Thumbnail too large. Should be less than 2Mb",
-        422
-      )
-    );
-  }
-  // rename the file
-  const newFileName = fileRename(thumbnail);
-  //   move the file into the uploads folder
-  thumbnail.mv(path.join(__dirname, "..", "uploads", newFileName), (err) => {
-    if (err) {
-      return next(new customErrorHandler(err));
-    }
-  });
-  // update the blog post with the new details
-  editedPost = await postModel.findByIdAndUpdate(
-    id,
-    {
-      title,
-      description,
-      category,
-      thumbnail: newFileName,
-    },
-    { new: true }
-  );
-
-  if (!editedPost) {
-    return next(new customErrorHandler("Could not update post", 422));
-  }
-
-  res.status(200).json(editedPost);
+  res
+    .status(400)
+    .json({ status: "error", msg: "You cannot edit someone else's post." });
 });
 
 // protected. api/posts/:id (delete)  ==================  DELETE POST
 const deletePost = asyncErrorHandler(async (req, res, next) => {
-  res.json("delete post");
+  const { id } = req.params;
+  // console.log(req.user.id);
+  if (!id) {
+    return next(new customErrorHandler("Please provide and id", 422));
+  }
+  // find the post with the specified id
+  const post = await postModel.findById(id);
+  if (!post) {
+    return next(new customErrorHandler("Post does not exist", 422));
+  }
+  // make sure that currently logged in user is the creator of the post with the id provided
+  if (req.user.id == post.creator) {
+    // delete the thumbnail
+    removeExistingAvatar(post, "blog");
+    // delete the post from the database
+    await postModel.findByIdAndDelete(id);
+    // find user and reduce the post count by 1
+    const user = await userModel.findById(req.user.id);
+    const postCount = user.no_of_posts - 1;
+    await userModel.findByIdAndUpdate(req.user.id, { no_of_posts: postCount });
+
+    return res
+      .status(200)
+      .json({ status: "sucess", msg: "post deleted successfully!" });
+  }
+
+  res.status(400).json({
+    status: "error",
+    msg: "You can not delete someone else's post",
+  });
 });
 
 module.exports = {

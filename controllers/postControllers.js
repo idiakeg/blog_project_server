15f27d7c8c37e53bafb2f87969c7d3291f57c38f
idiakeg <idiakeg@gmail.com>
2024-04-18 22:05:09 +0100

    Minor changes

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -9,218 +9,229 @@ const fileRename = require("../utils/fileRename");
 
 // protected. api/posts =================== CREATE POSTS
 const createPost = asyncErrorHandler(async (req, res, next) => {
-  const { title, description, category } = req.body;
-  const thumbnail = req.files?.thumbnail;
-
-  if (!thumbnail) {
-    return next(new customErrorHandler("Please include a thumbnail", 422));
-  }
-
-  //   check the size of the thumbnail
-  if (thumbnail.size > 2000000) {
-    return next(
-      new customErrorHandler(
-        "thumbnail too big. File size should be less than 2Mb.",
-        422
-      )
-    );
-  }
-
-  //   rename the file to ensure unique naming of files in the DB
-  let fileName = thumbnail.name;
-  let splittedFileName = fileName.split(".");
-  let newFileName =
-    splittedFileName[0] +
-    uuid() +
-    "." +
-    splittedFileName[splittedFileName.length - 1];
-
-  // move the thumbnail into the upload folder
-  thumbnail.mv(
-    path.join(__dirname, "..", "uploads", newFileName),
-    async (err) => {
-      if (err) {
-        return next(new customErrorHandler(err));
-      }
-    }
-  );
-
-  //  create the post
-  const post = await postModel.create({
-    title,
-    description,
-    category,
-    thumbnail: newFileName,
-    creator: req.user.id,
-  });
-
-  if (!post) {
-    return next(new customErrorHandler("Post could not be created.", 422));
-  }
-
-  //   increse post count for user theat created the post
-  const user = await userModel.findById(req.user.id);
-  const postCount = user.no_of_posts + 1;
-  // update the user post count with the new post count
-  await userModel.findByIdAndUpdate(req.user.id, {
-    no_of_posts: postCount,
-  });
-
-  res.status(201).json(post);
-});
-
-// Unprotected. api/posts ================== GET ALL POSTS
-const getPosts = asyncErrorHandler(async (req, res, next) => {
-  const posts = await postModel.find().sort({ updatedAt: 1 }); // -1: descending(the one that was created or updated last will or most recently will appear at the top). 1: ascending
-  res.status(200).json(posts);
-});
-
-// Unprotected. api/posts/:id ================== GET SINGLE POST
-const getSinglePosts = asyncErrorHandler(async (req, res, next) => {
-  const { id } = req.params;
-  const post = await postModel.findById(id);
-  if (!post) {
-    return next(new customErrorHandler("Post not found.", 422));
-  }
-  res.status(200).json(post);
-});
-
-// Unprotected. api/posts/categories/:category  ================== GET POST BY CATEGORY
-const getPostByCategory = asyncErrorHandler(async (req, res, next) => {
-  const { category } = req.params;
-  const categoryPost = await postModel
-    .find({ category })
-    .sort({ updatedAt: -1 });
-  res.status(200).json(categoryPost);
-});
-
-// Unprotected. api/posts/user/:id  ================== GET POST BY AUTHOR
-const getPostByAuthor = asyncErrorHandler(async (req, res, next) => {
-  //   obtain the id of the author
-  const { id } = req.params;
-  const authorPosts = await postModel
-    .find({ creator: id })
-    .sort({ updatedAt: -1 });
-  res.status(200).json(authorPosts);
-});
-
-// protected. api/posts/:id (patch)  ==================  EDIT POST
-const editPost = asyncErrorHandler(async (req, res, next) => {
-  const { id } = req.params;
-  const { title, description, category } = req.body;
-  const thumbnail = req.files?.thumbnail;
-  let editedPost;
-
-  // because of the react quill that is being used, a decription length of less than 12 characters means that the field is empty. React quill has a pararaph opening and closing tag with a break tag, so there are 11 characters in there already.
-  if (!title || !description || description.length < 12 || !category) {
-    return next(new customErrorHandler("All fields are required.", 422));
-  }
-
-  // get the actual blog post
-  const oldBlogPost = await postModel.findById(id);
-  // make sure that a user can only delete a post that they created.
-  if (req.user.id == oldBlogPost.creator) {
-    // make it possible to update a blog post without updating the thumbnail
+    const { title, description, category } = req.body;
+    const thumbnail = req.files?.thumbnail;
 
     if (!thumbnail) {
-      editedPost = await postModel.findByIdAndUpdate(
-        id,
-        { title, category, description },
-        { new: true }
-      );
-
-      if (!editedPost) {
-        return next(new customErrorHandler("Could not update post", 422));
-      }
-
-      return res.status(200).json(editedPost);
+        return next(new customErrorHandler("Please include a thumbnail", 422));
     }
 
-    // if the user wants to also edit the thumbnail of the blog post
-    // delete the old thumbnail
-    removeExistingAvatar(oldBlogPost, "blog");
-    // upload a new thumbnail
-    // check file size
+    //   check the size of the thumbnail
     if (thumbnail.size > 2000000) {
-      return next(
-        new customErrorHandler(
-          "Thumbnail too large. Should be less than 2Mb",
-          422
-        )
-      );
+        return next(
+            new customErrorHandler(
+                "thumbnail too big. File size should be less than 2Mb.",
+                422
+            )
+        );
     }
-    // rename the file
-    const newFileName = fileRename(thumbnail);
-    //   move the file into the uploads folder
-    thumbnail.mv(path.join(__dirname, "..", "uploads", newFileName), (err) => {
-      if (err) {
-        return next(new customErrorHandler(err));
-      }
-    });
-    // update the blog post with the new details
-    editedPost = await postModel.findByIdAndUpdate(
-      id,
-      {
+
+    //   rename the file to ensure unique naming of files in the DB
+    let fileName = thumbnail.name;
+    let splittedFileName = fileName.split(".");
+    let newFileName =
+        splittedFileName[0] +
+        uuid() +
+        "." +
+        splittedFileName[splittedFileName.length - 1];
+
+    // move the thumbnail into the upload folder
+    thumbnail.mv(
+        path.join(__dirname, "..", "uploads", newFileName),
+        async (err) => {
+            if (err) {
+                return next(new customErrorHandler(err));
+            }
+        }
+    );
+
+    //  create the post
+    const post = await postModel.create({
         title,
         description,
         category,
         thumbnail: newFileName,
-      },
-      { new: true }
-    );
+        creator: req.user.id,
+    });
 
-    if (!editedPost) {
-      return next(new customErrorHandler("Could not update post", 422));
+    if (!post) {
+        return next(new customErrorHandler("Post could not be created.", 422));
     }
 
-    return res.status(200).json(editedPost);
-  }
+    //   increse post count for user theat created the post
+    const user = await userModel.findById(req.user.id);
+    const postCount = user.no_of_posts + 1;
+    // update the user post count with the new post count
+    await userModel.findByIdAndUpdate(req.user.id, {
+        no_of_posts: postCount,
+    });
 
-  res
-    .status(400)
-    .json({ status: "error", msg: "You cannot edit someone else's post." });
+    res.status(201).json(post);
+});
+
+// Unprotected. api/posts ================== GET ALL POSTS
+const getPosts = asyncErrorHandler(async (req, res, next) => {
+    const posts = await postModel
+        .find()
+        .sort({ updatedAt: 1 })
+        .populate("creator"); // -1: descending(the one that was created or updated last or most recently will appear at the top). 1: ascending
+    res.status(200).json({ status: "success", posts });
+});
+
+// Unprotected. api/posts/:id ================== GET SINGLE POST
+const getSinglePosts = asyncErrorHandler(async (req, res, next) => {
+    const { id } = req.params;
+    const post = await postModel.findById(id).populate("creator");
+    if (!post) {
+        return next(new customErrorHandler("Post not found.", 422));
+    }
+    res.status(200).json({ status: "success", post });
+});
+
+// Unprotected. api/posts/categories/:category  ================== GET POST BY CATEGORY
+const getPostByCategory = asyncErrorHandler(async (req, res, next) => {
+    const { category } = req.params;
+    const categoryPost = await postModel
+        .find({ category })
+        .sort({ updatedAt: -1 });
+    res.status(200).json(categoryPost);
+});
+
+// Unprotected. api/posts/user/:id  ================== GET POST BY AUTHOR
+const getPostByAuthor = asyncErrorHandler(async (req, res, next) => {
+    //   obtain the id of the author
+    const { id } = req.params;
+    const authorPosts = await postModel
+        .find({ creator: id })
+        .sort({ updatedAt: -1 });
+    res.status(200).json(authorPosts);
+});
+
+// protected. api/posts/:id (patch)  ==================  EDIT POST
+const editPost = asyncErrorHandler(async (req, res, next) => {
+    const { id } = req.params;
+    const { title, description, category } = req.body;
+    const thumbnail = req.files?.thumbnail;
+    let editedPost;
+
+    // because of the react quill that is being used, a decription length of less than 12 characters means that the field is empty. React quill has a pararaph opening and closing tag with a break tag, so there are 11 characters in there already.
+    if (!title || !description || description.length < 12 || !category) {
+        return next(new customErrorHandler("All fields are required.", 422));
+    }
+
+    // get the actual blog post
+    const oldBlogPost = await postModel.findById(id);
+    // make sure that a user can only delete a post that they created.
+    if (req.user.id == oldBlogPost.creator) {
+        // make it possible to update a blog post without updating the thumbnail
+
+        if (!thumbnail) {
+            editedPost = await postModel.findByIdAndUpdate(
+                id,
+                { title, category, description },
+                { new: true }
+            );
+
+            if (!editedPost) {
+                return next(
+                    new customErrorHandler("Could not update post", 422)
+                );
+            }
+
+            return res.status(200).json({ status: "success", editedPost });
+        }
+
+        // if the user wants to also edit the thumbnail of the blog post
+        // delete the old thumbnail
+        removeExistingAvatar(oldBlogPost, "blog");
+        // upload a new thumbnail
+        // check file size
+        if (thumbnail.size > 2000000) {
+            return next(
+                new customErrorHandler(
+                    "Thumbnail too large. Should be less than 2Mb",
+                    422
+                )
+            );
+        }
+        // rename the file
+        const newFileName = fileRename(thumbnail);
+        //   move the file into the uploads folder
+        thumbnail.mv(
+            path.join(__dirname, "..", "uploads", newFileName),
+            (err) => {
+                if (err) {
+                    return next(new customErrorHandler(err));
+                }
+            }
+        );
+        // update the blog post with the new details
+        editedPost = await postModel.findByIdAndUpdate(
+            id,
+            {
+                title,
+                description,
+                category,
+                thumbnail: newFileName,
+            },
+            { new: true }
+        );
+
+        if (!editedPost) {
+            return next(new customErrorHandler("Could not update post", 422));
+        }
+
+        return res.status(200).json({ status: "success", editedPost });
+    }
+
+    res.status(400).json({
+        status: "error",
+        msg: "You cannot edit someone else's post.",
+    });
 });
 
 // protected. api/posts/:id (delete)  ==================  DELETE POST
 const deletePost = asyncErrorHandler(async (req, res, next) => {
-  const { id } = req.params;
-  // console.log(req.user.id);
-  if (!id) {
-    return next(new customErrorHandler("Please provide and id", 422));
-  }
-  // find the post with the specified id
-  const post = await postModel.findById(id);
-  if (!post) {
-    return next(new customErrorHandler("Post does not exist", 422));
-  }
-  // make sure that currently logged in user is the creator of the post with the id provided
-  if (req.user.id == post.creator) {
-    // delete the thumbnail
-    removeExistingAvatar(post, "blog");
-    // delete the post from the database
-    await postModel.findByIdAndDelete(id);
-    // find user and reduce the post count by 1
-    const user = await userModel.findById(req.user.id);
-    const postCount = user.no_of_posts - 1;
-    await userModel.findByIdAndUpdate(req.user.id, { no_of_posts: postCount });
+    const { id } = req.params;
+    // console.log(req.user.id);
+    if (!id) {
+        return next(new customErrorHandler("Please provide and id", 422));
+    }
+    // find the post with the specified id
+    const post = await postModel.findById(id);
+    if (!post) {
+        return next(new customErrorHandler("Post does not exist", 422));
+    }
+    // make sure that currently logged in user is the creator of the post with the id provided
+    if (req.user.id == post.creator) {
+        // delete the thumbnail
+        removeExistingAvatar(post, "blog");
+        // delete the post from the database
+        await postModel.findByIdAndDelete(id);
+        // find user and reduce the post count by 1
+        const user = await userModel.findById(req.user.id);
+        const postCount = user.no_of_posts - 1;
+        await userModel.findByIdAndUpdate(req.user.id, {
+            no_of_posts: postCount,
+        });
 
-    return res
-      .status(200)
-      .json({ status: "sucess", msg: "post deleted successfully!" });
-  }
+        return res
+            .status(200)
+            .json({ status: "sucess", msg: "post deleted successfully!" });
+    }
 
-  res.status(400).json({
-    status: "error",
-    msg: "You can not delete someone else's post",
-  });
+    res.status(400).json({
+        status: "error",
+        msg: "You can not delete someone else's post",
+    });
 });
 
 module.exports = {
-  createPost,
-  getPosts,
-  getSinglePosts,
-  getPostByCategory,
-  getPostByAuthor,
-  editPost,
-  deletePost,
+    createPost,
+    getPosts,
+    getSinglePosts,
+    getPostByCategory,
+    getPostByAuthor,
+    editPost,
+    deletePost,
 };
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -12,177 +12,183 @@ const removeExistingAvatar = require("../utils/removeExistingAvatar");
 // post request || /api/users/register || unprotected
 // ----> Register new user
 const regisiterUser = asyncErrorHandler(async (req, res, next) => {
-  const newUser = await userModel.create(req.body);
+    const newUser = await userModel.create(req.body);
 
-  return res.status(201).json({
-    status: "success",
-    data: {
-      user: newUser,
-    },
-  });
+    return res.status(201).json({
+        status: "success",
+        data: {
+            user: newUser,
+        },
+    });
 });
 
 // post request || /api/users/login || unprotected
 // -----> login a registered user, after a new user is registered, they are directed to then login
 const loginUser = asyncErrorHandler(async (req, res, next) => {
-  const { email, password } = req.body;
+    const { email, password } = req.body;
 
-  // the token will be sent to the client after a successful login
-  if (!email || !password) {
-    let error = new customErrorHandler(
-      "Please provide email and password for login",
-      400
-    );
-    return next(error);
-  }
-
-  // check if the a user with the provided email exists in the database
-  const user = await userModel.findOne({ email }).select("+password"); //here, we do get the user's password, because we need it to verify that the user provided the right information on login. This is okay because in the response we would not actually be sending the user object, but in other routes where the select will not be included, the user's password will not be visible
-
-  const isMatch = await user?.comparePassword(password, user.password);
-
-  if (!user || !isMatch) {
-    let error = new customErrorHandler("Incorrect email or password", 400);
-    return next(error);
-  }
-
-  const token = jwt.sign(
-    { id: user._id, name: user.name },
-    process.env.SECRET_STRING,
-    {
-      expiresIn: process.env.EXPIRES_IN,
+    // the token will be sent to the client after a successful login
+    if (!email || !password) {
+        let error = new customErrorHandler(
+            "Please provide email and password for login",
+            400
+        );
+        return next(error);
     }
-  );
 
-  res.status(200).json({
-    status: "success",
-    token,
-  });
+    // check if the a user with the provided email exists in the database
+    const user = await userModel.findOne({ email }).select("+password"); //here, we do get the user's password, because we need it to verify that the user provided the right information on login. This is okay because in the response we would not actually be sending the user object, but in other routes where the select will not be included, the user's password will not be visible
+
+    const isMatch = await user?.comparePassword(password, user.password);
+
+    if (!user || !isMatch) {
+        let error = new customErrorHandler("Incorrect email or password", 400);
+        return next(error);
+    }
+
+    const token = jwt.sign(
+        { id: user._id, name: user.name },
+        process.env.SECRET_STRING,
+        {
+            expiresIn: process.env.EXPIRES_IN,
+        }
+    );
+
+    const { password: userPassword, ...userWithoutPassword } = user._doc;
+
+    res.status(200).json({
+        status: "success",
+        token,
+        user: userWithoutPassword,
+    });
 });
 
 // get request || /api/users/:id || protected
 // -----> get the profile of a registered user
 const getUser = asyncErrorHandler(async (req, res, next) => {
-  const { id } = req.params;
-  const user = await userModel.findById(id);
+    const { id } = req.params;
+    const user = await userModel.findById(id);
 
-  if (!user) {
-    const error = new customErrorHandler("User not found!", 404);
-    return next(error);
-  }
-  res.status(200).json({ user });
+    if (!user) {
+        const error = new customErrorHandler("User not found!", 404);
+        return next(error);
+    }
+    res.status(200).json({ user });
 });
 
 // post request || /api/users/change_avatar || protected
 // ------> Change user avatar
 const changeAvatar = asyncErrorHandler(async (req, res, next) => {
-  const image = req.files.avatar;
+    const image = req.files.avatar;
 
-  if (!image) {
-    const error = new customErrorHandler("Please provide an image", 422);
-    next(error);
-    return;
-  }
-
-  // find the user in the database
-  const user = await userModel.findById(req.user.id); //recall that req.user was set in the protect mosule and it equals the payload present in the JWT, which in this case is the name and id of the user
-
-  // if an avatar exists, delete it.
-  if (user.avatar) {
-    removeExistingAvatar(user);
-  }
-
-  //check the file size of the avatar. note 1000bytes =1kb, 500000bytes = 500kb.
-  if (image.size > 500000) {
-    return next(
-      new customErrorHandler(
-        "Profile picture too big. Should be less than 500kb",
-        422
-      )
-    );
-  }
-
-  // if the file size is within range, change the name of the file, to ensure uniqueness.
-  const newFileName = fileRename(image);
-
-  // upload the file. use the mv function to move
-  image.mv(path.join(__dirname, "..", "uploads", newFileName), async (err) => {
-    if (err) {
-      return next(new customErrorHandler(err));
+    if (!image) {
+        const error = new customErrorHandler("Please provide an image", 422);
+        next(error);
+        return;
     }
-  });
 
-  const updatedAvatar = await userModel.findByIdAndUpdate(
-    req.user.id,
-    {
-      avatar: newFileName,
-    },
-    { new: true }
-  );
+    // find the user in the database
+    const user = await userModel.findById(req.user.id); //recall that req.user was set in the protect mosule and it equals the payload present in the JWT, which in this case is the name and id of the user
 
-  if (!updatedAvatar) {
-    return next(new customErrorHandler(err));
-  }
+    // if an avatar exists, delete it.
+    if (user.avatar) {
+        removeExistingAvatar(user);
+    }
 
-  res.status(200).json(updatedAvatar);
+    //check the file size of the avatar. note 1000bytes =1kb, 500000bytes = 500kb.
+    if (image.size > 500000) {
+        return next(
+            new customErrorHandler(
+                "Profile picture too big. Should be less than 500kb",
+                422
+            )
+        );
+    }
+
+    // if the file size is within range, change the name of the file, to ensure uniqueness.
+    const newFileName = fileRename(image);
+
+    // upload the file. use the mv function to move
+    image.mv(
+        path.join(__dirname, "..", "uploads", newFileName),
+        async (err) => {
+            if (err) {
+                return next(new customErrorHandler(err));
+            }
+        }
+    );
+
+    const updatedAvatar = await userModel.findByIdAndUpdate(
+        req.user.id,
+        {
+            avatar: newFileName,
+        },
+        { new: true }
+    );
+
+    if (!updatedAvatar) {
+        return next(new customErrorHandler(err));
+    }
+
+    res.status(200).json(updatedAvatar);
 });
 
 // post request || /api/users/edit_user || protected
 // ------> Edit user details
 const editUser = asyncErrorHandler(async (req, res, next) => {
-  const { name, email, currentPassword, newPassword, newConfirmPassword } =
-    req.body;
-  if (!email || !name || !currentPassword || !newPassword) {
-    return next(new customErrorHandler("Fill in all fields.", 422));
-  }
-  // find the user based on the id provided by the protect middleware
-  const user = await userModel.findById(req.user.id).select("+password");
-  if (!user) {
-    return next(new customErrorHandler("User not found.", 403));
-  }
+    const { name, email, currentPassword, newPassword, newConfirmPassword } =
+        req.body;
+    if (!email || !name || !currentPassword || !newPassword) {
+        return next(new customErrorHandler("Fill in all fields.", 422));
+    }
+    // find the user based on the id provided by the protect middleware
+    const user = await userModel.findById(req.user.id).select("+password");
+    if (!user) {
+        return next(new customErrorHandler("User not found.", 403));
+    }
 
-  // make sure a user with the provded email doesnot exist
-  const emailExists = await userModel.findOne({ email });
-  if (emailExists && emailExists._id.toString() !== req.user.id) {
-    //essentially checks if a user with the provided id exists and whether or not it is the logged in user. this will prevent one user from changing the email of another user
-    return next(new customErrorHandler("Email already  exist.", 422));
-  }
+    // make sure a user with the provded email doesnot exist
+    const emailExists = await userModel.findOne({ email });
+    if (emailExists && emailExists._id.toString() !== req.user.id) {
+        //essentially checks if a user with the provided id exists and whether or not it is the logged in user. this will prevent one user from changing the email of another user
+        return next(new customErrorHandler("Email already  exist.", 422));
+    }
 
-  // compare passwords
-  const isMatch = await user.comparePassword(currentPassword, user.password); //this is here to ensure that for a user to change her password, they must first provide the currentpassword
-  if (!isMatch) {
-    return next(new customErrorHandler("Invalid current password.", 422));
-  }
+    // compare passwords
+    const isMatch = await user.comparePassword(currentPassword, user.password); //this is here to ensure that for a user to change her password, they must first provide the currentpassword
+    if (!isMatch) {
+        return next(new customErrorHandler("Invalid current password.", 422));
+    }
 
-  // compare new password and current password
-  if (newPassword !== newConfirmPassword) {
-    return next(new customErrorHandler("New password does not match", 422));
-  }
+    // compare new password and current password
+    if (newPassword !== newConfirmPassword) {
+        return next(new customErrorHandler("New password does not match", 422));
+    }
 
-  // hash the password:
-  const hashedPassword = await bcrypt.hash(newPassword, 10);
+    // hash the password:
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
 
-  // update user information
-  const updatedUser = await userModel.findByIdAndUpdate(
-    req.user.id,
-    { name, email, password: hashedPassword },
-    { new: true }
-  );
+    // update user information
+    const updatedUser = await userModel.findByIdAndUpdate(
+        req.user.id,
+        { name, email, password: hashedPassword },
+        { new: true }
+    );
 
-  res.status(200).json(updatedUser);
+    res.status(200).json(updatedUser);
 });
 
 // get request || /api/users/ || unprotected
 // ------> Get all authors/users
 const getAuthors = (req, res) => {
-  res.json("get all authors");
+    res.json("get all authors");
 };
 
 module.exports = {
-  getAuthors,
-  editUser,
-  changeAvatar,
-  loginUser,
-  getUser,
-  regisiterUser,
+    getAuthors,
+    editUser,
+    changeAvatar,
+    loginUser,
+    getUser,
+    regisiterUser,
 };
